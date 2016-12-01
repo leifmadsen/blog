b381a7491aee4ddf58b08433ea61e66caf14df50
Leif Madsen <lmadsen@redhat.com>
2016-12-01 14:50:18 -0500

    Move some git subtree stuff around in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Makde sure everything is sane
+git subtree pull --prefix=public git@github.com:leifmadsen/blog.git gh-pages
+git pull
+
 # Build the project.
 hugo
 
@@ -17,6 +21,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git pull
-git subtree pull --prefix=public git@github.com:leifmadsen/blog.git gh-pages
 git subtree push --prefix=public git@github.com:leifmadsen/blog.git gh-pages
