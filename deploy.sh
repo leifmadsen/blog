9b42066a4b4f8bc6fbe6b9f611921f713afb216f
Leif Madsen <lmadsen@redhat.com>
2016-12-30 09:54:39 -0500

    Make it easier to tweak the deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,21 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+pull () {
+    git pull
+}
+
+push () {
+    git push origin master
+}
+
+subtree_pull () {
+    git subtree pull --prefix=public git@github.com:leifmadsen/blog.git gh-pages
+}
+
+subtree_push () {
+    git subtree push --prefix=public git@github.com:leifmadsen/blog.git gh-pages
+}
 
 # Build the project.
 hugo
@@ -17,11 +32,11 @@ fi
 git commit -m "$msg"
 
 # Make sure everything is sane
-git subtree pull --squash --prefix=public git@github.com:leifmadsen/blog.git gh-pages
-git pull
+subtree_pull
+pull
 
 # Push source and build repos.
-git push origin master
-git subtree push --prefix=public git@github.com:leifmadsen/blog.git gh-pages
-git pull
-git subtree pull --squash --prefix=public git@github.com:leifmadsen/blog.git gh-pages
+push
+subtree_push
+pull
+subtree_pull
